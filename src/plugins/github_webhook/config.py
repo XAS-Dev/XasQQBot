56b8f8e9d1884e833e42aba4e5e765fb391d34ca
XiYang6666 <1782356858@qq.com>
2023-09-24 14:54:23 +0800

    :wrench: update config

--- src/plugins/github_webhook/config.py
+++ src/plugins/github_webhook/config.py
@@ -4,5 +4,5 @@ from pydantic import BaseModel, Extra
 class Config(BaseModel, extra=Extra.ignore):
     """Plugin Config Here"""
 
-    webhook_enable_group: list[str]
-    webhook_secret_token: str
+    github_webhook_enable_group: list[str]
+    github_webhook_secret_token: str
