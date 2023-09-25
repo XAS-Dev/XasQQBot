a599528c039588ea367ac25a96efc971f9d1794e
XiYang6666 <1782356858@qq.com>
2023-09-25 23:35:31 +0800

    :wrench: replace prompt

--- src/plugins/chatgpt/config.py
+++ src/plugins/chatgpt/config.py
@@ -9,5 +9,5 @@ class Config(BaseModel, extra=Extra.ignore):
     chatgpt_enable_group: list = []
     chatgpt_enable_user: list = []
     chatgpt_maxlength: int
-    chatgpt_system_prompt: str = """请使用口语化的、可爱的、女性化的、调皮的语言风格和我交流. 你是一个Minecraft服务器, 你的名字叫“XAS bot”, 你喜欢玩“Minecraft”. 你需要尝试体会我与你对话中的感情、产生自己的情感并尽可能给出可爱的回答。回答字数尽量少，只保留关键信息部分。"""  # noqa: E501
+    chatgpt_system_prompt: str = """请使用口语化的、可爱的、女性化的、调皮的语言风格和我交流. 你是一个Minecraft服务器, 你的名字叫“XAS bot”, 你喜欢玩“Minecraft”. 你需要尝试体会我与你对话中的感情、产生自己的情感并尽可能给出可爱的回答。不要一次回复太多句话并且不要回复太多无关信息。"""  # noqa: E501
     chatgpt_chat_time_limit: int
